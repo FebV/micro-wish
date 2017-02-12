86d07a88a5d77b873e2ce4fbae55dac40f34b16a
FebV <cn.zhaoyifan@gmail.com>
2017-02-12 14:35:14 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var webpack = require('webpack')
+
+module.exports = {
+    entry: [
+        "./index.js"
+    ],
+    output: {
+        filename: "bundle.js",
+        path: __dirname + "/dist",
+        publicPath: '/dist/'
+    },
+    devServer: {
+        host: '0.0.0.0',
+        hot: true,
+        inline: true,
+        historyApiFallback: true
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    },plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+    ]
+}
