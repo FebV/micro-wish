c433c7e644dbbaca18b110d07a4494e6b0db2e93
FebV <cn.zhaoyifan@gmail.com>
2017-02-22 15:12:28 +0800

    fix wrong param

--- components/WishList.js
+++ components/WishList.js
@@ -139,7 +139,7 @@ class AcceptDialog extends React.Component {
         })
             .then(res => res.json())
             .then(res => {
-                if(res.statue == 200) {
+                if(res.status == 200) {
                     const msg = res == 0 ? '接受失败' : `接受成功, 发布者的联系方式是${res.data}`;
                     alert(msg);
                     dispatchEvent(new Event('refreshMyWish'));
